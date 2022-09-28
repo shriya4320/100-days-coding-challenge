6c4b84cec53370879a2237821997dc2b63c6d55a
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-09-28 23:16:40 +0530

    Add files via upload

---
+++ Prac1.java
@@ -0,0 +1,29 @@
+class Prac1{
+
+public static void main(string args[])
+{
+	int a=20;
+	System.out.println("value of a"+a);
+
+	short b=10;
+	System.out.println("value of b"+b);
+
+	long c=30;
+	System.out.println("value of c"+c);
+
+	float d=35.5F;
+	System.out.println("value of d"+d);
+
+	double e=234;
+	System.out.println("value of e"+e);
+
+	char f='D';
+	System.out.println("value of f"+f);
+	
+	boolean g=true;
+	System.out.println("value of g"+g);
+
+	System.out.println("Hello");
+
+			}
+}
