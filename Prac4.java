2aa49f96231cd792985174d0efa40b6f25606e6e
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-09-30 23:25:08 +0530

    Add files via upload

---
+++ Prac4.java
@@ -0,0 +1,21 @@
+//odd or even//
+
+class Prac4{
+public static voidn main(string args[])
+{
+    int a=5;
+
+    if(a%2==0)
+    {
+        System.out.println("a is even");
+    }
+
+    else
+    {
+        System.out.println("a is odd");
+    }
+    
+   
+}
+
+}
