0602c4c023139d6413b2db0118cae75b3422ac3f
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-09-30 23:58:43 +0530

    Add files via upload

---
+++ Prac6.java
@@ -0,0 +1,35 @@
+//vowel or not//
+
+class Prac6{
+public static void main(String args[])
+{
+String vowel="a";
+switch(vowel){
+    case "a" :
+    System.out.println("a is a vowel");
+    break;
+
+    case "e" :
+    System.out.println("e is a vowel");
+    break;
+
+    case "i" :
+    System.out.println("i is a vowel");
+    break;
+    
+    case "o" :
+    System.out.println("o is a vowel");
+    break;
+
+    case "u" :
+    System.out.println("u is a vowel");
+    break;
+
+    default:
+    System.out.println("vowel+" is not a vowel);
+
+}
+
+
+}
+}
