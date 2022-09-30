99efa23d0b811f07b31627e9d4fad57c62cb9518
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-09-30 23:40:25 +0530

    Add files via upload

---
+++ Prac5.java
@@ -0,0 +1,18 @@
+// positive or negative//
+
+  class Prac5{
+    public static void main(string args[])
+    {
+        int a=30;
+
+        if(a>=0)
+        {
+            System.out.println("a is positive");
+        }
+
+        else
+        {
+            System.out.println("a is negatuve");
+        }
+    }
+}
