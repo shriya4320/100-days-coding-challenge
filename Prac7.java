4eefb63045dfdc3e284cc1a63f3c4d07e5da9230
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-10-03 23:15:53 +0530

    Add files via upload

---
+++ Prac7.java
@@ -0,0 +1,14 @@
+//addition of 1 to 10//
+class Prac7{
+    public static void main()
+    {
+        int a;
+        int i=0;
+
+        for(a=1; a<=10; a++)
+        {
+           i+=a;
+        }
+        System.out.println(i+"is total");
+    }
+}
