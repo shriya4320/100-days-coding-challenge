1b9cf00b7180114a0da45d5ca44934bb4a43c1e3
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-10-07 00:20:41 +0530

    Add files via upload

---
+++ Prac17.java
@@ -0,0 +1,13 @@
+class Prac17{
+    public static void main(String args[]){
+        for(int i=1; i<=7; i+=2){
+            for(int j=1; j<=i; j++){
+                if(j%2=1)
+                {
+                    System.out.print(j);
+                }
+            }
+                    System.out.println();
+        }
+    }
+}
