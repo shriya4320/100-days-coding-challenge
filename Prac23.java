671221666dd0ee2b350afde9682da704be771328
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-10-10 22:41:13 +0530

    Add files via upload

---
+++ Prac23.java
@@ -0,0 +1,11 @@
+class Prac23{
+    public static void main(String args[]){
+        for(int i=65; i<=70; i++){
+            for(int j= 65; j<=i; j++){
+                System.out.print((char)j);
+            }
+                System.out.println();
+        }
+
+    }
+}
