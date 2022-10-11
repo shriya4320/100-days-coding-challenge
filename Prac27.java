88c03832af5fe91b2396ffd7df04cd5a1ab61f00
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-10-11 23:44:39 +0530

    Add files via upload

---
+++ Prac27.java
@@ -0,0 +1,8 @@
+class Prac27{
+    public static void main(String args[]){
+        int a[]={5,6,7,8,9};
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i]);
+        }
+    }
+}
