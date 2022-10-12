87edf2c903e9150659225cde56886b2be7c079aa
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-10-13 00:16:21 +0530

    Add files via upload

---
+++ Prac33.java
@@ -0,0 +1,12 @@
+class Prac33{
+    static void area(){
+        double r,pi,a;
+        r=2;
+        pi=3.142;
+        a=pi*r*r;
+        System.out.print("area:"+a);
+    }
+        public static void main(String args[]){
+        area();
+        }
+}
