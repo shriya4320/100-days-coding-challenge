e6f378280aa63529e1fb3f4533d3f3715f479607
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-10-13 00:13:36 +0530

    Add files via upload

---
+++ Prac32.java
@@ -0,0 +1,12 @@
+class Prac32{
+    static void add(){
+        int a,b,c;
+        a=10;
+        b=20;
+        c=a+b;
+        System.out.print("Add:"+c);
+    }
+        public static void main(String args[]){
+        add();
+        }
+}
