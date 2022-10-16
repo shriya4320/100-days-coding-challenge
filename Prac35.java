7562d86b5df9ce3eeff36c997436da55141a01dc
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-10-16 21:49:28 +0530

    Add files via upload

---
+++ Prac35.java
@@ -0,0 +1,12 @@
+class Prac35{
+    public static void main(String args[]){
+        int x,y;
+        int ans=1;
+        System.out.println("Enter x:");
+        System.out.println("Enter y:");
+            for(int i=1; i<=y; i++){
+            ans=ans*x;
+         }
+         System.out.println("ans:");
+   }
+}
