6d4a1d60934e64908b557597c5d84db86e392b0a
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-10-23 23:41:06 +0530

    Add files via upload

---
+++ Prac40.java
@@ -0,0 +1,11 @@
+class Prac40{
+    public static void main(String args[]){
+        int b[]={3,5,9,11,7,13};
+        int sum=0;
+
+            for(int i=0; i<b.length; i++){
+                sum= sum+b[i];
+            }
+            System.out.print("sum of the elements:"+sum+"");
+    }
+}
