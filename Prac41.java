3c4277a2989ca509b373f1e6a155a096f6efc8e0
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-10-24 00:10:22 +0530

    Add files via upload

---
+++ Prac41.java
@@ -0,0 +1,11 @@
+class Prac41{
+    static void area(){
+        double s,a;
+        s=4;
+        a=s*s;
+        System.out.print("area:"+a);
+    }
+        public static void main(String args[]){
+        area();
+        }
+}
