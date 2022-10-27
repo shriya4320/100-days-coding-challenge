2e51fc5890df7b26a55471d4f0addc273a7835a0
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-10-28 00:41:30 +0530

    Add files via upload

---
+++ Prac45.java
@@ -0,0 +1,17 @@
+
+class Prac45{
+    public static void main(string args[])
+    {
+        int a=-25;
+
+        if(a>=0)
+        {
+            System.out.println("a is positive integer");
+        }
+
+        else
+        {
+            System.out.println("a is negative integer");
+        }
+    }
+}
