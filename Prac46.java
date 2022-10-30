456583a3790a85c782cfcc221b1807f56eb8b320
Shriya Thakur <102856369+shriya4320@users.noreply.github.com>
2022-10-30 23:27:13 +0530

    Add files via upload

---
+++ Prac46.java
@@ -0,0 +1,10 @@
+class Prac46{
+    public static void main(String args[]{
+        double r,v,h;
+        r=5;
+        h=10;
+        pi=3.142;
+        v=pi*r*h;
+        System.out.print("volume:"+v);
+    }
+}
